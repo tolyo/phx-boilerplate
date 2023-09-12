b11c9b1da9fc94380146fbf76c082ba5d0c95366
tolyo <anatolyostrovsky@gmail.com>
2023-09-12 19:40:49 +0300

    Fix func call

--- lib/web/shared.ex
+++ lib/web/shared.ex
@@ -2,5 +2,5 @@ defmodule Web.Shared do
   import Nitroux.Utils
 
   @spec server_page(String.t()) :: String.t()
-  def server_page(url), do: "server-page" |> tag(%{url: url})
+  def server_page(url), do: "server-page" |> tag(url: url)
 end

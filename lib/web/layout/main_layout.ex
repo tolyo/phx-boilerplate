b9cd2d838f540a0e5235c1a0f059cae2cb54eba0
tolyo <anatolyostrovsky@gmail.com>
2023-09-18 23:05:52 +0300

    Fix live reload broken due to missing tag

--- lib/web/layout/main_layout.ex
+++ lib/web/layout/main_layout.ex
@@ -1,5 +1,22 @@
 defmodule MainLayout do
+  import Nitroux
   use Web, :controller
+
+  @doc """
+  Optional wrapper helper. Default to content/2 as it allows to use layout plugs
+  """
+  def wrap(content) do
+    "<!DOCTYPE html>" <>
+      html(
+        lang: Gettext.get_locale(Web.Gettext),
+        translate: "no",
+        html: [
+          LayoutHelper.header(),
+          content |> body(),
+          LayoutHelper.footer()
+        ]
+      )
+  end
 
   def content(conn, data) do
     conn

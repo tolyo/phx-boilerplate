ccd469cd017a661a0774c82d0b2fbb1de8101270
tolyo <anatolyostrovsky@gmail.com>
2023-09-20 13:19:19 +0300

    Add router types

--- lib/web/@types/global.d.ts
+++ lib/web/@types/global.d.ts
@@ -1,8 +1,11 @@
+import { StateService, UIRouter } from "@uirouter/core";
 import { Alpine } from "alpinejs";
 
 declare global {
   interface Window {
     Alpine: Alpine;
+    router: UIRouter;
+    stateService: StateService;
   }
 }
 

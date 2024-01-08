eadd729bc2c856ce89e13c79520ad3153fa94c33
Anatoly Ostrovsky <anatolyostrovsky@gmail.com>
2024-01-08 19:49:53 +0200

    Sync frontend '

--- lib/web/global.d.ts
+++ lib/web/global.d.ts
@@ -1,11 +1,14 @@
 import { StateService, UIRouter } from "@uirouter/core";
 import { Alpine } from "alpinejs";
-import FormController from "../utils/form-controller";
+import FormController from "./utils/form-controller";
+import { RouteConfig } from "./utils/router";
 
 declare global {
   interface Window {
     Alpine: Alpine;
     router: UIRouter;
+    routes: RouteConfig[];
+    crudRoutes: RouteConfig[];
     stateService: StateService;
     EventBus: EventTarget;
     FormControllers: Array<FormController>;
